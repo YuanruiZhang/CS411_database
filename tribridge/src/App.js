8b52777fd8c5b9979c265a8b189266e90cf93927
YuanruiZhang <yuanrz@uw.edu>
2021-12-01 20:13:06 +0800

    basic interface v1 complete

--- tribridge/src/App.js
+++ tribridge/src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [patDescription, setpatDescription] = useState('');
   const [sePatRow, setsePatRow]  = useState([]);
 
-
+  // patient
   const insertPatient = (patfname, patLname, patDoB, patGender, patAddr, patState, patEmail, patDescription) => { 
     Axios.post('http://localhost:3002/patient/insert', {
       patfname: patfname,
@@ -36,31 +36,6 @@ function App() {
     })
   };
 
-    // Doctor
-    const [docID, setDocID] = useState('');
-    const [docFname, setDocfname] = useState('');
-    const [docLname, setDocLname] = useState('');
-    const [docAffil, setDocAffil] = useState('');
-    const [docEmail, setDocEmail] = useState('');
-    const [seDocRow, setseDocRow]  = useState([]);
-  
-  
-    const insertDoctor = (docFname, docLname, docAffil, docEmail) => { 
-      Axios.post('http://localhost:3002/Doctors/insert', {
-        docFname: docFname,
-        docLname: docLname,
-        docAffil: docAffil,
-        docEmail: docEmail
-      }).then((response) => {
-        setseDocRow(response.data)
-      })
-    };
-
-  // SELECT
-  // const [sePatFname, setsePatFname] = useState('');
-  // const [sePatLname, setsePatLname] = useState('');
-  // const [sePatEmail, setsePatEmail] = useState('');
-
   const selectPatient = (patFname,patLname) => {
     Axios.post('http://localhost:3002/patient/search', {
       patFname: patFname,
@@ -73,25 +48,6 @@ function App() {
      
     })
   };
-
-  const selectDoctor = (docFname,docLname) => {
-    Axios.post('http://localhost:3002/Doctors/search', {
-      docFname: docFname,
-      docLname: docLname,
-    })
-    .then((response) => {
-      if (response.data != []){
-        setseDocRow(response.data[0])
-      }
-    })
-  };
-
-
-  // UPDATES
-  // const [upPatfname, setupPatfname] = useState('');
-  // const [upPatLname, setupPatLname] = useState('');
-  // const [upPatID, setupPatID] = useState('');
-  // const [upPatNewEmail, setupPatNewEmail] = useState('');
 
   const updatePatient = (patID, patfname, patLname, patDoB, Gender, Address, State, patEmail, Description) => {
     Axios.put(`http://localhost:3002/patient/update`, {
@@ -109,40 +65,152 @@ function App() {
     })
   };
 
-  // const [upDocID, setupDocID] = useState('');
-  // const [upDocNewEmail, setupDocNewEmail] = useState('');
-  // const [upDocNewAfill, setupDocNewAfill] = useState('');
-
-  const updateDoctor = (docID, docEmail,docAffil) => {
-    Axios.put(`http://localhost:3002/Doctors/update`, {
-      doctorID: docID,
-      docNewEmail: docEmail,
-      docNewAffli: docAffil
-    }).then((response) => {
-      setseDocRow(response.data)
-    })
-  };
-
-  
-
-
-  // DELETE
-  // const [delPatID, setdelPatID] = useState('');
   const deletePatient = (patID) => {
     Axios.post(`http://localhost:3002/patient/delete`, {
       patID: patID
     });
   };
 
-  const deleteDoctor = (docID) => {
-    Axios.post(`http://localhost:3002/Doctors/delete`, {
-      docID: docID
-    });
-  };
+    // Doctor
+    const [docID, setDocID] = useState('');
+    const [docFname, setDocfname] = useState('');
+    const [docLname, setDocLname] = useState('');
+    const [docAffil, setDocAffil] = useState('');
+    const [docEmail, setDocEmail] = useState('');
+    const [seDocRow, setseDocRow]  = useState([]);
+  
+    const insertDoctor = (docFname, docLname, docAffil, docEmail) => { 
+      Axios.post('http://localhost:3002/Doctors/insert', {
+        docFname: docFname,
+        docLname: docLname,
+        docAffil: docAffil,
+        docEmail: docEmail
+      }).then((response) => {
+        setseDocRow(response.data)
+      })
+    };
+    
+    const selectDoctor = (docFname,docLname) => {
+      Axios.post('http://localhost:3002/Doctors/search', {
+        docFname: docFname,
+        docLname: docLname,
+      })
+      .then((response) => {
+        if (response.data != []){
+          setseDocRow(response.data[0])
+        }
+      })
+    };
 
-  // const deleteReview = (movieName) => {
-  //   Axios.delete(`http://localhost:3002/api/delete/${movieName}`);
-  // };
+    const updateDoctor = (docID, docEmail,docAffil) => {
+      Axios.put(`http://localhost:3002/Doctors/update`, {
+        doctorID: docID,
+        docNewEmail: docEmail,
+        docNewAffli: docAffil
+      }).then((response) => {
+        setseDocRow(response.data)
+      })
+    };
+
+    const deleteDoctor = (docID) => {
+      Axios.post(`http://localhost:3002/Doctors/delete`, {
+        docID: docID
+      });
+    };
+
+    // trial
+    const [trialID, settrialID] = useState('');
+    const [trialTitle, settrialTitle] = useState('');
+    const [trialMedCID, settrialMedCID] = useState('');
+    const [trialDescription, settrialDescription] = useState('');
+    const [trialRow, settrialRow]  = useState([]);
+
+    const insertTrial = (trialTitle, trialMedCID, trialDescription) => { 
+      Axios.post('http://localhost:3002/trial/insert', {
+        title: trialTitle,
+        MedCompID: trialMedCID,
+        Description: trialDescription
+      }).then((response) => {
+        settrialRow(response.data)
+      })
+    };
+
+    const selectTrial = (trialTitle) => {
+      Axios.post('http://localhost:3002/trial/search', {
+        title: trialTitle,
+      })
+      .then((response) => {
+        if (response.data != []){
+          settrialRow(response.data[0])
+        }
+      })
+    };
+
+    const updateTrial = (trialID, trialMedCID, trialDescription) => {
+      Axios.put(`http://localhost:3002/trial/update/`, {
+        trialID: trialID,
+        MedCompID: trialMedCID,
+        Description: trialDescription
+      }).then((response) => {
+        settrialRow(response.data)
+      })
+    };
+
+    const deleteTrial = (trialMedCID) => {
+      Axios.post(`http://localhost:3002/trial/delete`, {
+        MedCID: trialMedCID
+      });
+    };
+
+    // Report
+    const [reportID, setreportID] = useState('');
+    const [reportpatID, setreportpatID] = useState('');
+    const [reportdocID, setreportdocID] = useState('');
+    const [reporttrialID, setreporttrialID] = useState('');
+    const [reportdate, setreportdate] = useState('');
+    const [reportRow, setReportRow]  = useState([]);
+
+    const insertReport = (reportpatID, reportdocID, reporttrialID,reportdate) => { 
+      Axios.post('http://localhost:3002/report/insert', {
+        patID: reportpatID,
+        docID: reportdocID,
+        trialID: reporttrialID,
+        date: reportdate
+      }).then((response) => {
+        setReportRow(response.data)
+      })
+    };
+
+    const selectReport = (reportpatID) => {
+      Axios.post('http://localhost:3002/report/search', {
+        patientID: reportpatID,
+      })
+      .then((response) => {
+        if (response.data != []){
+          setReportRow(response.data[0])
+        }
+      })
+    };
+
+    const updateReport = (reportID,reportpatID,reportdocID,reporttrialID, reportdate) => {
+      Axios.put(`http://localhost:3002/report/update/`, {
+        reportID: reportID,
+        patID: reportpatID,
+        docID: reportdocID,
+        trialID: reporttrialID,
+        date: reportdate,
+      }).then((response) => {
+        setReportRow(response.data)
+      })
+    };
+
+    const deleteReport = (reportID) => {
+      Axios.post(`http://localhost:3002/report/delete`, {
+        reportID: reportID
+      });
+    };    
+
+
 
 
   // advance query 1
@@ -267,6 +335,19 @@ function App() {
                   updateDoctor(docID,docEmail, docAffil)
           }}> Update</button>
         </div>
+
+        <div className = "card">
+        <h1> Delete Doctor:  </h1>
+        <p>id:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                setDocID(e.target.value)
+        } }/>
+
+        <button onClick={() => {
+                deleteDoctor(patID)
+        }}> delete</button>
+      </div>
+
       </div>
 
       <div className = "horizBox">
@@ -386,10 +467,185 @@ function App() {
                   updatePatient(docID,patFname, patLname, patDOB, patGender, patAddr, patState, patEmail, patDescription,)
           }}> Update</button>
         </div>
+
+        <div className = "cardPatient">
+        <h1> Delete Patient:  </h1>
+        <p>id:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                setpatID(e.target.value)
+        } }/>
+
+        <button onClick={() => {
+                deletePatient(patID)
+        }}> delete</button>
+      </div>
+    </div>
+
+
+    
+    <div className = "horizBox">
+      <div className = "card">
+        <h2>Reflecting Trial</h2>
+        <ol id="listView">
+        <li>trialID: {trialRow.trialID}</li>
+        <li>trialTitle: {trialRow.trialTitle}</li>
+        <li>trialMedCID: {trialRow.trialMedCID}</li>
+        <li>trialDescription: {trialRow.trialDescription}</li>
+        </ol>
       </div>
 
+      <div className = "card">
+      <h1> SELECT Trial:  </h1>
+      <p>Trial Title:</p>
+      <input type="text" id="patSelectInput" onChange={(e) => {
+              settrialTitle(e.target.value)
+      } }/>
 
+      <button onClick={() => {
+              selectTrial(trialTitle)
+      }}> SELECT</button>
+      </div>
 
+      <div className = "card">
+        <h1> INSERT Trial:  </h1>
+        <p>Trial title:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialTitle(e.target.value)
+        } }/>
+        <p>Trial MedCompID:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialMedCID(e.target.value)
+        } }/>
+        <p>Trial Description:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialDescription(e.target.value)
+        } }/>
+        <button onClick={() => {
+                insertTrial(trialTitle, trialMedCID, trialDescription)
+        }}> INSERT</button>
+      </div>
+
+      <div className = "card">
+        <h1> Update Trial:  </h1>
+        <p>id:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialID(e.target.value)
+        } }/>
+        <p>new med company ID:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialMedCID(e.target.value)
+        } }/>
+        <p>new trial Description:</p>
+        <input type="text" id="patSelectInput" onChange={(e) => {
+                settrialDescription(e.target.value)
+        } }/>
+
+        <button onClick={() => {
+                updateTrial(trialID, trialMedCID, trialDescription)
+        }}> Update</button>
+      </div>
+
+      <div className = "card">
+      <h1> Delete Trial:  </h1>
+      <p>id:</p>
+      <input type="text" id="patSelectInput" onChange={(e) => {
+              settrialID(e.target.value)
+      } }/>
+
+      <button onClick={() => {
+              deleteTrial(patID)
+      }}> delete</button>
+    </div>
+    </div>
+
+<div className = "horizBox">
+
+<div className = "card">
+  <h2>Reflecting Report</h2>
+  <ol id="listView">
+  <li>reportID: {trialRow.reportID}</li>
+  <li>reportpatID: {trialRow.reportpatID}</li>
+  <li>reportdocID: {trialRow.reportdocID}</li>
+  <li>reporttrialID: {trialRow.reporttrialID}</li>
+  </ol>
+</div>
+
+<div className = "card">
+<h1> SELECT Report:  </h1>
+<p>Trial Title:</p>
+<input type="text" id="patSelectInput" onChange={(e) => {
+        setreportpatID(e.target.value)
+} }/>
+
+<button onClick={() => {
+        selectReport(reportpatID)
+}}> SELECT</button>
+</div>
+
+<div className = "card">
+  <h1> INSERT Report:  </h1>
+  <p>Report patID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportpatID(e.target.value)
+  } }/>
+  <p>Report docID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportdocID(e.target.value)
+  } }/>
+  <p>Report trialID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreporttrialID(e.target.value)
+  } }/>
+  <p>Report Date:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportdate(e.target.value)
+  } }/>
+  <button onClick={() => {
+          insertTrial(trialTitle, trialMedCID, trialDescription)
+  }}> INSERT</button>
+</div>
+
+<div className = "card">
+  <h1> Update Report:  </h1>
+  <p>Report ID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportID(e.target.value)
+  } }/>
+  <p>Report patID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportpatID(e.target.value)
+  } }/>
+  <p>Report docID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportdocID(e.target.value)
+  } }/>
+  <p>Report trialID:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreporttrialID(e.target.value)
+  } }/>
+  <p>Report Date:</p>
+  <input type="text" id="patSelectInput" onChange={(e) => {
+          setreportdate(e.target.value)
+  } }/>
+
+  <button onClick={() => {
+          updateReport(reportID,reportpatID,reportdocID,reporttrialID, reportdate)
+  }}> Update</button>
+</div>
+
+<div className = "card">
+<h1> Delete Report:  </h1>
+<p>id:</p>
+<input type="text" id="patSelectInput" onChange={(e) => {
+        settrialID(e.target.value)
+} }/>
+
+<button onClick={() => {
+        deleteTrial(patID)
+}}> delete</button>
+</div>
+
+</div>
 
       <div className = "card">
         <h1> Update Doctor:  </h1>
@@ -452,20 +708,6 @@ function App() {
                 updatePatient(patID, patFname, patLname, patDOB, patGender, patAddr, patState, patEmail, patDescription)
         }}> Update</button>
       </div>
-
-      <div className = "card">
-        <h1> Delete Patient:  </h1>
-        <p>id:</p>
-        <input type="text" id="patSelectInput" onChange={(e) => {
-                setpatID(e.target.value)
-        } }/>
-
-        <button onClick={() => {
-                deletePatient(patID)
-        }}> delete</button>
-
-      </div>
-
 
       <div className = "card">
         <h1> Patient per Company </h1>
